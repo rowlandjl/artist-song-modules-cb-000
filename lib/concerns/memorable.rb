5805f84cec652f602571d9f1618755a8adc91848
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 16:36:15 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
-  
-  def reset_all
-    self.all.clear 
-  end 
-  
-  def count 
-    self.all.count 
-  end 
+  module ClassMethods 
+    def reset_all
+      self.all.clear 
+    end 
+    
+    def count 
+      self.all.count 
+    end 
   
 end 
